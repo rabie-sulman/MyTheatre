7ce228631b83733655d8dc9ed7b3380c0551c779
Pavel Rumyantsev <prumyantsev@encore.co.uk>
2020-06-22 14:41:05 +0300

    DPW-7360 Introduce widgetized version of MyTheatre

---
+++ Widgets/controllers/basket.js
@@ -0,0 +1,292 @@
+const { URL } = require('url');
+var request = require('request-promise');
+var xml2js = require('xml2js');
+const staticCustomerData = require("../customerData.json");
+
+
+var xmlBuilder = new xml2js.Builder();
+var parser = new xml2js.Parser();
+
+var authURI = 'booking/authenticate';
+var basketURI = 'booking/basket';
+var bookingURI = 'booking/book';
+
+const getAuthString = (apiCredentials) => {
+    return Buffer.from(apiCredentials.affiliateId + ':' + apiCredentials.affiliatePassword);
+}
+const addToBasket = (host, inputs, apiCredentials, template, callback) => {
+    var authBody = getAuthBody(apiCredentials);
+    var authString = getAuthString(apiCredentials);
+    var url = new URL(authURI, host);
+    request({
+        "method":"POST", 
+        "uri": url.toString(),
+        "json": false,
+        "headers": {
+            "content-type": "application/xml",
+            "Authorization": "Basic " + authString.toString('base64')
+        },
+        body: authBody
+    }).then(function (data) {
+        parser.parseString(data, function (err, result){
+            var addToBasketBody = getAddToBasketBody(inputs, apiCredentials, result.agent.session);
+            var url = new URL(basketURI, host);
+            request({
+                "method":"POST", 
+                "uri": url.toString(),
+                "json": false,
+                "headers": {
+                    "content-type": "application/xml",
+                    "Authorization": "Basic " + authString.toString('base64')
+                },
+                body: addToBasketBody
+            }).then(function (data){
+                parser.parseString(data, function (error, result){
+                    console.log(JSON.stringify(result));
+                    callback.render(template, {
+                        basketDetails: result,
+                        customer: staticCustomerData,
+                        title: 'Basket page',
+                        subtitle: 'Basket details:',
+                    });
+                });
+            }).catch(function(err){
+                console.log(err.message);
+                parser.parseString(err, function (error, result){
+                    console.log(result);
+                });
+                callback.render("error", {
+                    title: 'Basket page',
+                    messages: ["error in add to basket - try again"],
+                })
+            });
+        });
+    }).catch(function (err) {
+        console.log(err.message);
+        callback.render("error", {
+            title: 'Basket page',
+            messages: ["error in auth - try again"],
+        })
+    });
+}
+
+const deleteBasket = (host, inputs, apiCredentials, template, callback) => {
+    var authBody = getAuthBody(apiCredentials);
+    var authString = getAuthString(apiCredentials);
+    var url = new URL(authURI, host);
+    request({
+        "method":"POST", 
+        "uri": url.toString(),
+        "json": false,
+        "headers": {
+            "content-type": "application/xml",
+            "Authorization": "Basic " + authString.toString('base64')
+        },
+        body: authBody
+    }).then(function (data) {
+        parser.parseString(data, function (err, result){
+            var deleteBasketBody = getDeleteBasketBody(inputs, apiCredentials, result.agent.session);
+            var url = new URL(basketURI, host);
+            request({
+                "method":"DELETE", 
+                "uri": url.toString(),
+                "json": false,
+                "headers": {
+                    "content-type": "application/xml",
+                    "Authorization": "Basic " + authString.toString('base64')
+                },
+                body: deleteBasketBody
+            }).then(function (data){
+                console.log("deleted");
+                callback.render(template, {
+                    title: 'Basket page',
+                    messages: ["booking: <" + inputs.reference + "> deleted"],
+                })
+            }).catch(function(err){
+                console.log(err.message);
+                callback.render(template, {
+                    title: 'Basket page',
+                    messages: ["unable to delete booking: <" + inputs.reference + ">"],
+                })
+            });
+        });
+    }).catch(function (err) {
+        console.log(err.message);
+        callback.render("error", {
+            title: 'Basket page',
+            messages: ["error in auth - try again"],
+        })
+    });
+}
+
+const createBooking = (host, inputs, apiCredentials, template, callback) => {
+    var authBody = getAuthBody(apiCredentials);
+    var authString = getAuthString(apiCredentials);
+    var url = new URL(authURI, host);
+    request({
+        "method":"POST", 
+        "uri": url.toString(),
+        "json": false,
+        "headers": {
+            "content-type": "application/xml",
+            "Authorization": "Basic " + authString.toString('base64')
+        },
+        body: authBody
+    }).then(function (data) {
+        parser.parseString(data, function (err, result){
+            var createBookingBody = getCreateBookingBody(inputs, apiCredentials, result.agent.session, staticCustomerData);
+            var url = new URL(bookingURI, host);
+            request({
+                "method":"POST", 
+                "uri": url.toString(),
+                "json": false,
+                "headers": {
+                    "content-type": "application/xml",
+                    "Authorization": "Basic " + authString.toString('base64')
+                },
+                body: createBookingBody
+            }).then(function (data){
+                console.log("booked!");
+                callback.render(template, {
+                    title: 'Basket page',
+                    messages: ["booking: <" + inputs.reference + "> has been successful!"],
+                })
+            }).catch(function(err){
+                console.log(err.message);
+                callback.render(template, {
+                    title: 'Basket page',
+                    messages: ["unable to delete booking: <" + inputs.reference + ">"],
+                })
+            });
+        });
+    }).catch(function (err) {
+        console.log(err.message);
+        callback.render("error", {
+            title: 'Basket page',
+            messages: ["error in auth - try again"],
+        })
+    });
+}
+
+const getAuthBody = (apiCredentials) => {
+    return xmlBuilder.buildObject({
+        agent: {
+            $: {
+                id: apiCredentials.agentId
+            },
+            password: apiCredentials.agentPassword
+        }
+    });
+}
+const getDeleteBasketBody = (inputs, apiCredentials, session) => {
+    return xmlBuilder.buildObject({
+        basket: {
+            transaction: {
+                $: {
+                    reference: inputs.reference
+                },
+                password: inputs.password,
+            },
+            reservations: {
+                reservation: {
+                    $: {
+                        id: 1
+                    }
+                }
+            },
+            agent: {
+                $: {
+                    id: apiCredentials.agentId
+                },
+                session: session,
+                partner: apiCredentials.affiliateId    
+            },
+        }
+    });
+}
+
+const getCreateBookingBody = (inputs, apiCredentials, session, customer) => {
+    return xmlBuilder.buildObject({
+        booking: {
+            transaction: {
+                $: {
+                    reference: inputs.reference
+                },
+                password: inputs.password,
+            },
+            customer: {
+                title: customer.title,
+                firstName: customer.title,
+                lastName: customer.lastName,
+                email: customer.email,
+                address: {
+                    $: {
+                        type: customer.address.type,
+                    },
+                    line1: customer.address.line1,
+                    line2: customer.address.line2,
+                    city: customer.address.city,
+                    county: customer.address.county,
+                    postcode: customer.address.postcode,
+                    country: customer.address.country,
+                },
+                phone: customer.phone,
+                mail: {
+                    $: {
+                        type: customer.mail.type,
+                    },
+                }
+            },
+            agent: {
+                $: {
+                    id: apiCredentials.agentId
+                },
+                session: session,
+                partner: apiCredentials.affiliateId    
+            },
+        }
+    }); 
+}
+const getAddToBasketBody = (inputs, apiCredentials, session) => {
+    var seatKey = new Buffer(inputs.seatKey, 'base64');
+    seatKey = seatKey.toString('ascii');    
+    return xmlBuilder.buildObject({
+        basket: {
+            product: {
+                $: {
+                    id: inputs.productId
+                },
+                type: 'show',
+                venue: {
+                    $: {
+                        id: inputs.venueId
+                    },
+                },
+                performance: {
+                    $: {
+                        type: 'A'
+                    },
+                },
+                date: inputs.date,
+                quantity: inputs.ticketQuantity,
+                startFrom: inputs.startFrom,
+                seat: {
+                    $: {
+                        key: seatKey
+                    }
+                }
+            },
+            agent: {
+                $: {
+                    id: apiCredentials.agentId
+                },
+                session: session,
+                partner: apiCredentials.affiliateId    
+            },
+        }
+    });
+}
+
+module.exports.addToBasket = addToBasket;
+module.exports.deleteBasket = deleteBasket;
+module.exports.createBooking = createBooking;
---
+++ Widgets/index.js
@@ -0,0 +1,152 @@
+/**
+ * Required External Modules
+ */
+const express = require('express');
+const qs = require('qs');
+const path = require('path');
+const moment = require('moment');
+const basket = require('./controllers/basket');
+
+/**
+ * App Variables
+ */
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('query parser', function (str) {
+    return qs.parse(str, { decoder: function (s) { return decodeURIComponent(s); } });
+});
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ *  App Configuration
+ */
+const port = process.env.PORT || '8000';
+var config = require('./config');
+app.set(config, config);
+
+/**
+ * Routes Definitions
+ */
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home', subtitle: 'Start your journey, here!' });
+});
+
+app.get('/product-external', (req, res) => {
+    const inputs = config.inputs;
+    const inventorySettings = config.settings.inventory;
+    const configuration = {
+        productId: inputs.productId,
+        venueId: inputs.venueId,
+        quantity: inputs.quantity,
+        fromDate: moment().toString('YYYYMMDD'),             // today
+        toDate: moment().add(1, 'weeks').format('YYYYMMDD'), // a week from now
+        productType: inputs.productType,
+        affiliateId: config.settings.affiliateId,
+        apiPath: inventorySettings.host,
+        widgetVersion: inventorySettings.widgetVersion,
+    };
+
+    res.render('product-external', {
+        configuration,
+        title: 'Product page',
+        subtitle: 'Choose date:'
+    });
+});
+
+app.get('/product', (req, res) => {
+    const inputs = config.inputs;
+    const inventorySettings = config.settings.inventory;
+    const configuration = {
+        productId: inputs.productId,
+        venueId: inputs.venueId,
+        quantity: inputs.quantity,
+        fromDate: moment().toString('YYYYMMDD'),             // today
+        toDate: moment().add(1, 'weeks').format('YYYYMMDD'), // a week from now
+        productType: inputs.productType,
+        affiliateId: config.settings.affiliateId,
+        apiPath: inventorySettings.host,
+        widgetVersion: inventorySettings.widgetVersion,
+    };
+
+    res.render('product', {
+        configuration,
+        title: 'Product page',
+        subtitle: 'Choose date:'
+    });
+});
+
+app.get('/seating-plan-external', (req, res) => {
+    const venueSettings = config.settings.venue;
+    const configuration = {
+        channelId: config.settings.channelId,
+        apiPath: venueSettings.host,
+        widgetVersion: venueSettings.widgetVersion,
+        actionUrl: venueSettings.redirectUrl,
+    };
+
+    res.render('seating-plan-external', {
+        configuration,
+        title: 'Seat Plan page',
+        subtitle: 'Choose seats:',
+    });
+});
+
+app.get('/seating-plan', (req, res) => {
+    const venueSettings = config.settings.venue;
+    const configuration = {
+        channelId: config.settings.channelId,
+        apiPath: venueSettings.host,
+        widgetVersion: venueSettings.widgetVersion,
+    };
+
+    res.render('seating-plan', {
+        configuration,
+        title: 'Seat Plan page',
+        subtitle: 'Choose seats:',
+    });
+});
+
+app.get('/addToBasket', (req, res) => {
+    var host = config.env.eapi.host;
+    var apiCredentials = config.env.eapi;
+    var venueId = config.inputs.venueId;
+    var date = req.query.date + 'T' + req.query.time;
+    var addToBasketInputs = {
+        productId: req.query.productId,
+        ticketQuantity: req.query.quantity,
+        seatKey: req.query.seatKey,
+        date: moment(date).format('YYYY-MM-DD'),
+        time: moment(date).format('HH:mm'),
+        startFrom: req.query.number,
+        venueId: venueId,
+    };
+    basket.addToBasket(host, addToBasketInputs, apiCredentials, 'basket', res);
+});
+
+app.get('/deleteBasket', (req, res) => {
+    var host = config.env.eapi.host;
+    var apiCredentials = config.env.eapi;
+    var deleteBasketInputs = {
+        reference: req.query.reference,
+        password: req.query.password,
+    };
+    basket.deleteBasket(host, deleteBasketInputs, apiCredentials, 'index', res);
+});
+
+app.get('/createBooking', (req, res) => {
+    var host = config.env.eapi.host;
+    var apiCredentials = config.env.eapi;
+    var createBooking = {
+        reference: req.query.reference,
+        password: req.query.password,
+    };
+    basket.createBooking(host, createBooking, apiCredentials, 'booking', res);
+});
+
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
